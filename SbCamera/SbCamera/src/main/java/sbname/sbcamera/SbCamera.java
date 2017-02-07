d72c06b6fabdc7b69b6f03b5068b4284cfef337c
Jerry Tao <jerry.tao@musical.ly>
2017-02-07 23:46:14 +0800

    WindPush$ extend some simple paramters for SbCamera

--- SbCamera/SbCamera/src/main/java/sbname/sbcamera/SbCamera.java
+++ SbCamera/SbCamera/src/main/java/sbname/sbcamera/SbCamera.java
@@ -1,36 +1,69 @@
 package sbname.sbcamera;
 
+import android.graphics.ImageFormat;
 import android.hardware.Camera;
+import android.os.Build;
 import android.view.SurfaceView;
 
 import java.io.IOException;
+import java.util.List;
 
 import sbname.sbcamera.listener.SbCameraListener;
+
+import static android.hardware.Camera.getCameraInfo;
+import static android.hardware.Camera.getNumberOfCameras;
 
 /**
  * Created by WindPush on 2017/2/6.
  */
 
 public class SbCamera {
+
     public static final int ERROR_CODE_CAMERA_OPEN_FAIL = -1;
     public static final int ERROR_CODE_CAMERA_PREVIEW_FAIL = -2;
 
-    private SbCameraListener mSbCameraListener;
+    private int mPreviewWidth;
+    private int mPreviewHeight;
 
+    private int mCurrentCameraId = -1;
+    private int mFrontCameraId = -1;
+    private int mBackCameraId = -1;
+    private int mTotalCameraNums = 0;
+
+    private SbCameraListener mSbCameraListener;
     private Camera mCamera;
+    private Camera.CameraInfo mCameraInfo;
+    private final int mImageFormat = ImageFormat.NV21;
 
     private static class SbCameraHolder {
         private static SbCamera INSTANCE = new SbCamera();
     }
 
     private SbCamera() {
+        initCameraInfo();
+    }
+
+    private void initCameraInfo() {
+        int numberOfCameras = getNumberOfCameras();
+        mTotalCameraNums = numberOfCameras;
+        mCameraInfo = new Camera.CameraInfo();
+        for (int i = 0; i < numberOfCameras; i++) {
+            getCameraInfo(i, mCameraInfo);
+            if (mCameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+                mBackCameraId = i;
+            }
+
+            if (mCameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                mFrontCameraId = i;
+            }
+        }
     }
 
     public static SbCamera getInstance() {
         return SbCameraHolder.INSTANCE;
     }
 
-    public void startPreviewWithSurface(SurfaceView surfaceView){
+    public void startPreviewWithSurface(SurfaceView surfaceView) {
         boolean openSuccess = openCamera();
         if (!openSuccess) {
             if (mSbCameraListener != null) {
@@ -47,14 +80,45 @@ public class SbCamera {
             }
             e.printStackTrace();
         }
+
+        initDefaultParamsForCamera();
     }
+
+    private void initDefaultParamsForCamera() {
+        if (mCamera == null) {
+            return;
+        }
+        Camera.Parameters parameters = mCamera.getParameters();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            parameters.setRecordingHint(true);
+        }
+
+        parameters.setPreviewFormat(mImageFormat);
+
+        //set focus mode
+        final List<String> focusModes = parameters.getSupportedFocusModes();
+        if (focusModes.contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO)) {
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
+        } else if(focusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+        } else {
+        }
+
+        mCamera.setParameters(parameters);
+    }
+
+    private void adjustRotationForCamera() {
+
+    }
+
 
     private boolean openCamera() {
         if (mCamera == null) {
             mCamera = Camera.open();
         }
 
-        return mCamera == null ;
+        return mCamera == null;
     }
 
     public void release() {
@@ -71,4 +135,19 @@ public class SbCamera {
         mSbCameraListener = sbCameraListener;
     }
 
+    public int getPreviewWidth() {
+        return mPreviewWidth;
+    }
+
+    public void setPreviewWidth(int previewWidth) {
+        mPreviewWidth = previewWidth;
+    }
+
+    public int getPreviewHeight() {
+        return mPreviewHeight;
+    }
+
+    public void setPreviewHeight(int previewHeight) {
+        mPreviewHeight = previewHeight;
+    }
 }
