d28f0033eec1c9ec7d800cb50b66beb13d1f007f
Jerry Tao <jerry.tao@musical.ly>
2017-02-09 23:34:07 +0800

    change surface to glsurface

--- SbCamera/SbCamera/src/main/java/sbname/sbcamera/SbCameraView.java
+++ SbCamera/SbCamera/src/main/java/sbname/sbcamera/SbCameraView.java
@@ -1,28 +1,39 @@
 package sbname.sbcamera;
 
 import android.content.Context;
+import android.opengl.GLSurfaceView;
 import android.util.AttributeSet;
-import android.view.SurfaceView;
 
 /**
  * Created by WindPush on 2017/2/6.
+ * choose the glsurface in order to support with some cool feature like prisma style filter .scale or other interesting preview looks.
  */
 
-public class SbCameraView  extends SurfaceView implements SbCameraPresenter{
+public class SbCameraView  extends GLSurfaceView implements SbCameraPresenter{
     private int mPreviewWith = 0;
     private int mPreviewHeight= 0;
 
+    /**
+     * Standard View constructor. In order to render something, you
+     * must call {@link #setRenderer} to register a renderer.
+     *
+     * @param context
+     */
     public SbCameraView(Context context) {
         super(context);
     }
 
-    public SbCameraView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
-
+    /**
+     * Standard View constructor. In order to render something, you
+     * must call {@link #setRenderer} to register a renderer.
+     *
+     * @param context
+     * @param attrs
+     */
     public SbCameraView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
+
 
     @Override
     public SbCameraView setCameraPreviewSize(int previewWidth, int previewHeight) {
